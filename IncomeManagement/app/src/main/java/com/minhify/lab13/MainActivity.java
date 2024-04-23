99eb9757ab76857a141b4163030bc665b024f310
Mee18 <nguyenseoul18@gmail.com>
2024-04-23 17:46:27 +0700

    Add Class EnterActivity:

--- IncomeManagement/app/src/main/java/com/minhify/lab13/MainActivity.java
+++ IncomeManagement/app/src/main/java/com/minhify/lab13/MainActivity.java
@@ -1,24 +1,125 @@
 package com.minhify.lab13;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnlogin;
+    TextView btnregister, sendResetPasswd;
+    ImageButton getinfo;
+    EditText usname, uspasswd;
+    private FirebaseAuth mAuth;
 
+    FirebaseUser user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        usname = findViewById(R.id.txtLginUsername);
+        uspasswd = findViewById(R.id.txtLginPassword);
+        mAuth = FirebaseAuth.getInstance();
+        user = mAuth.getCurrentUser();
+        if (user == null){
+
+        }
+        else {
+            Intent intent = new Intent(getApplicationContext(), ListActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
+        btnlogin = findViewById(R.id.btnLogin);
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /*Intent intent = new Intent(MainActivity.this, ListActivity.class);
+                startActivity(intent);*/
+                String email, password;
+                email = String.valueOf(usname.getText());
+                password =String.valueOf(uspasswd.getText());
+
+                if(TextUtils.isEmpty(email)){
+                    Toast.makeText(MainActivity.this, "Enter email", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(TextUtils.isEmpty(password)){
+                    Toast.makeText(MainActivity.this, "Enter password",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(getApplicationContext(), "Login successful", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(MainActivity.this, ListActivity.class);
+                            startActivity(intent);
+                            finish();
+
+                        }else {
+                            Toast.makeText(MainActivity.this, "Authenication failed.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
+        btnregister = findViewById(R.id.toRegister);
+        btnregister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        getinfo = findViewById(R.id.btnInfoApp);
+        getinfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Application to manage Spending", Toast.LENGTH_SHORT).show();
+            }
+        });
+        sendResetPasswd = findViewById(R.id.sendResetPasswd);
+        sendResetPasswd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(TextUtils.isEmpty(usname.getText())){
+                    Toast.makeText(getApplicationContext(), "Please enter email to reset password!!!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                mAuth.sendPasswordResetEmail(usname.getText().toString())
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(getApplicationContext(), "We have sent you instructions to reset your password!", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(getApplicationContext(), "Failed to send reset email!", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
         });
     }
 }
